38ab8fea17aba2157ced47c5a8daa6ec9796153c
LaiCheong <122101351+LaiCheong@users.noreply.github.com>
2023-01-07 01:39:01 +1100

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   
   //fetch data from server 
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://try1-xw08.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
